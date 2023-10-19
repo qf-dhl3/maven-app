2f4fa7540ddf8f8947ffa7a175543506cd783908
ravi <ravi@vytec.com>
2023-10-19 14:44:18 +0530

    uodated

---
+++ sonarscanner.sh
@@ -0,0 +1,5 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=mavenapp \
+  -Dsonar.projectName='mavenapp' \
+  -Dsonar.host.url=http://3.110.218.203:9000 \
+  -Dsonar.token=sqp_76cea8d0b336fa82219eef26681388ae1b033ab6
